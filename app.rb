4bcb79696142f96ea8b4e20afb680e72bfc240c7
Alex Mamonchik <alex.mamonchik@gmail.com>
2018-05-22 03:44:08 +0600

    added config.ru

--- app.rb
+++ app.rb
@@ -8,31 +8,35 @@ require 'upwork/api/routers/auth'
 require 'upwork/api/routers/reports/time'
 require './upwork_helper'
 
-helpers UpworkApiWrapper
 
-get '/' do
-  begin
-    now = Date.today
-    monday = now - (now.wday - 1) % 7
-    sunday = now + (7 - (now.wday))
+class HoursApp < Sinatra::Base
 
-    report_response = report.get_by_freelancer_limited(
-      ENV['UPWORK_USERNAME'],
-      tqx: 'out:json',
-      tq: "select hours where worked_on >= '#{monday.strftime("%Y-%m-%d")}' AND worked_on <= '#{sunday.strftime("%Y-%m-%d")}' order by worked_on"
-    )
-    times = report_response['table']['rows'].map do |row|
-      if row['c'].is_a?(Array)
-        row['c'].map { |c| c['v'].to_f }.inject(:+)
-      else
-        row['c']['v'].to_f
+  helpers UpworkApiWrapper
+
+  get '/' do
+    begin
+      now = Date.today
+      monday = now - (now.wday - 1) % 7
+      sunday = now + (7 - (now.wday))
+
+      report_response = report.get_by_freelancer_limited(
+        ENV['UPWORK_USERNAME'],
+        tqx: 'out:json',
+        tq: "select hours where worked_on >= '#{monday.strftime("%Y-%m-%d")}' AND worked_on <= '#{sunday.strftime("%Y-%m-%d")}' order by worked_on"
+      )
+      times = report_response['table']['rows'].map do |row|
+        if row['c'].is_a?(Array)
+          row['c'].map { |c| c['v'].to_f }.inject(:+)
+        else
+          row['c']['v'].to_f
+        end
       end
+      hours = times.inject(:+).to_f.floor(1)
+    rescue => e
+      puts e.message
+      puts e.backtrace
     end
-    hours = times.inject(:+).to_f.floor(1)
-  rescue => e
-    puts e.message
-    puts e.backtrace
-  end
 
-  json hours: (hours || '...')
+    json hours: (hours || '...')
+  end
 end
